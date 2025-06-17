fcc4f77cce2c80058ac83f6d7ba7c17c6744a793
NamNoise <nam.hoang916@education.nsw.gov.au>
2025-06-17 03:40:45 +0000

    dsd

---
+++ .vscode/character_test.py
@@ -0,0 +1,16 @@
+from character import Character
+harry = Character("Harry", "A smelly Wumpus")
+harry.describe()
+
+# Add some conversation for Harry when he is talked to
+harry.set_conversation("Come closer. I can't see you!")
+
+# Trigger a conversation with Harry
+harry.talk()
+# Set a weakness
+harry.set_weakness("vegemite")
+
+# Fight with harry
+print("What will you fight with?")
+fight_with = input()
+harry.fight(fight_with)
--- .vscode/main.py
+++ .vscode/main.py
@@ -15,8 +15,26 @@ cavern.link_cave(dungeon, "south")
 grotto.link_cave(dungeon, "east")
 
 current_cave = cavern
-while True:
+dead = False
+while dead == False:
     print("\n")
     current_cave.get_details()
+    inhabitant = current_cave.get_character()
+    if inhabitant is not None:
+        inhabitant.describe()
     command = input(">")
     current_cave = current_cave.move(command)
+    if command in ["north", "south", "east", "west"]:
+        current_cave = current_cave.move(command)
+    elif command == "talk":
+        if inhabitant is not None:
+            inhabitant.talk()
+
+    elif command == "fight":
+        if inhabitant is not None and isinstance(inhabitant, Enemy): 
+
+            print("What will you fight with?")
+            fight_with = input()
+            if inhabitant.fight(fight_with) == True:
+                print("Bravo, hero you won the fight!")
+                current_cave.set_character(None)
